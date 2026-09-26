86cee7f1232f7b8cb809733356e052b8429ecd14
Nikolay Mishin <nikmarqa@gmail.com>
2026-09-26 20:24:00 +0300

    Fix tests

--- test.js
+++ test.js
@@ -106,7 +106,7 @@ export const test = async () => {
     //await ffmpeg(f10, postfix, true);
     //await ffmpeg(f12, postfix, true);
 
-    //await testScripts(117);
+    //await testScripts(120);
 
     //await scan(fd);
 
